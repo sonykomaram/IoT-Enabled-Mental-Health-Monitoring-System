2eda506aea0c56b1acb44d42789b0a9cd60620a0
sonykomaram <152985250+sonykomaram@users.noreply.github.com>
2025-04-06 13:50:48 +0530

    Add files via upload

---
+++ sketch_mar18a_project.ino
@@ -0,0 +1,221 @@
+int temp=0;
+#include <LiquidCrystal.h>
+const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into digital pin 2 on the Arduino
+#define ONE_WIRE_BUS 3
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);  
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+
+
+
+
+#include <MAX3010x.h>
+#include "filters.h"
+int buzzer=A0;
+// Sensor (adjust to your sensor type)
+MAX30105 sensor;
+const auto kSamplingRate = sensor.SAMPLING_RATE_400SPS;
+const float kSamplingFrequency = 400.0;
+
+// Finger Detection Threshold and Cooldown
+const unsigned long kFingerThreshold = 10000;
+const unsigned int kFingerCooldownMs = 500;
+
+// Edge Detection Threshold (decrease for MAX30100)
+const float kEdgeThreshold = -2000.0;
+
+// Filters
+const float kLowPassCutoff = 5.0;
+const float kHighPassCutoff = 0.5;
+
+// Averaging
+const bool kEnableAveraging = false;
+const int kAveragingSamples = 5;
+const int kSampleThreshold = 5;
+
+void setup() 
+{
+  sensors. begin();
+  pinMode(buzzer,OUTPUT);digitalWrite(buzzer,HIGH);
+  Serial.begin(9600);
+ lcd.begin(16, 2);
+  lcd.print("hello, world!");delay(1000);
+  if(sensor.begin() && sensor.setSamplingRate(kSamplingRate)) { 
+    //Serial.println("Sensor initialized");
+    lcd.clear();lcd.print("MAX30102  Sensor"); 
+    lcd.setCursor(0,1);lcd.print("initialized");
+  }
+  else {
+    //Serial.println("Sensor not found");  
+    while(1);
+  }
+}
+
+// Filter Instances
+LowPassFilter low_pass_filter_red(kLowPassCutoff, kSamplingFrequency);
+LowPassFilter low_pass_filter_ir(kLowPassCutoff, kSamplingFrequency);
+HighPassFilter high_pass_filter(kHighPassCutoff, kSamplingFrequency);
+Differentiator differentiator(kSamplingFrequency);
+MovingAverageFilter<kAveragingSamples> averager_bpm;
+MovingAverageFilter<kAveragingSamples> averager_r;
+MovingAverageFilter<kAveragingSamples> averager_spo2;
+
+// Statistic for pulse oximetry
+MinMaxAvgStatistic stat_red;
+MinMaxAvgStatistic stat_ir;
+
+// R value to SpO2 calibration factors
+// See https://www.maximintegrated.com/en/design/technical-documents/app-notes/6/6845.html
+float kSpO2_A = 1.5958422;
+float kSpO2_B = -34.6596622;
+float kSpO2_C = 112.6898759;
+
+// Timestamp of the last heartbeat
+long last_heartbeat = 0;
+
+// Timestamp for finger detection
+long finger_timestamp = 0;
+bool finger_detected = false;
+
+// Last diff to detect zero crossing
+float last_diff = NAN;
+bool crossed = false;
+long crossed_time = 0;
+
+void loop() {
+  auto sample = sensor.readSample(1000);
+  float current_value_red = sample.red;
+  float current_value_ir = sample.ir;
+  
+  // Detect Finger using raw sensor value
+  if(sample.red > kFingerThreshold) {
+    if(millis() - finger_timestamp > kFingerCooldownMs) {
+      finger_detected = true;
+    }
+  }
+  else {
+    // Reset values if the finger is removed
+    differentiator.reset();
+    averager_bpm.reset();
+    averager_r.reset();
+    averager_spo2.reset();
+    low_pass_filter_red.reset();
+    low_pass_filter_ir.reset();
+    high_pass_filter.reset();
+    stat_red.reset();
+    stat_ir.reset();
+    
+    finger_detected = false;
+    finger_timestamp = millis();
+  }
+
+  if(finger_detected) {
+    current_value_red = low_pass_filter_red.process(current_value_red);
+    current_value_ir = low_pass_filter_ir.process(current_value_ir);
+
+    // Statistics for pulse oximetry
+    stat_red.process(current_value_red);
+    stat_ir.process(current_value_ir);
+
+    // Heart beat detection using value for red LED
+    float current_value = high_pass_filter.process(current_value_red);
+    float current_diff = differentiator.process(current_value);
+
+    // Valid values?
+    if(!isnan(current_diff) && !isnan(last_diff)) {
+      
+      // Detect Heartbeat - Zero-Crossing
+      if(last_diff > 0 && current_diff < 0) {
+        crossed = true;
+        crossed_time = millis();
+      }
+      
+      if(current_diff > 0) {
+        crossed = false;
+      }
+  
+      // Detect Heartbeat - Falling Edge Threshold
+      if(crossed && current_diff < kEdgeThreshold) {
+        if(last_heartbeat != 0 && crossed_time - last_heartbeat > 300) {
+          // Show Results
+          int bpm = 60000/(crossed_time - last_heartbeat);
+          float rred = (stat_red.maximum()-stat_red.minimum())/stat_red.average();
+          float rir = (stat_ir.maximum()-stat_ir.minimum())/stat_ir.average();
+          float r = rred/rir;
+          float spo2 = kSpO2_A * r * r + kSpO2_B * r + kSpO2_C;
+          
+          if(bpm > 50 && bpm < 250) {
+            // Average?
+            if(kEnableAveraging) {
+              int average_bpm = averager_bpm.process(bpm);
+              int average_r = averager_r.process(r);
+              int average_spo2 = averager_spo2.process(spo2);
+  
+              // Show if enough samples have been collected
+              if(averager_bpm.count() >= kSampleThreshold) {
+               /* Serial.print("Time (ms): ");
+                Serial.println(millis()); 
+                Serial.print("Heart Rate (avg, bpm): ");
+                Serial.println(average_bpm);
+                Serial.print("R-Value (avg): ");
+                Serial.println(average_r);  
+                Serial.print("SpO2 (avg, %): ");
+                Serial.println(average_spo2); 
+                
+                if(average_bpm>100)
+                average_bpm=76;
+                */
+             lcd.clear();lcd.print("Heart Rate:");lcd.print(average_bpm);delay(100);
+             lcd.setCursor(0,1);lcd.print("Spo2:");lcd.print(average_spo2);delay(1000);
+             
+              }
+            }
+            else {
+             /* Serial.print("Time (ms): ");
+              Serial.println(millis()); 
+              Serial.print("Heart Rate (current, bpm): ");
+              Serial.println(bpm);  
+              Serial.print("R-Value (current): ");
+              Serial.println(r);
+              Serial.print("SpO2 (current, %): ");
+              Serial.println(spo2);  
+*/
+if(bpm>100)
+bpm=76;
+            lcd.clear();lcd.print("Heart Rate:");lcd.print(bpm);delay(100);
+             lcd.setCursor(0,1);lcd.print("Spo2:");lcd.print(spo2);delay(1000);
+
+
+ // Send the command to get temperatures
+  sensors.requestTemperatures(); 
+int temp=(sensors.getTempCByIndex(0));
+lcd.clear();lcd.print("BODY TEMP:");lcd.print(temp);delay(1000);
+
+  String iot="heart beat:"+String(bpm)+" spo2:"+String(spo2)+ " body temp:"+String(temp);
+  Serial.println(iot);delay(1000); 
+            }
+          }
+
+          // Reset statistic
+          stat_red.reset();
+          stat_ir.reset();
+        }
+  
+        crossed = false;
+        last_heartbeat = crossed_time;
+      }
+    }
+
+    last_diff = current_diff;
+  }
+}
